83c1aeceb37ce1253e62b6cc857ed0774f3f3de8
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-02-25 10:46:14 +0100

    Revert "video: mdss: LiveDisplay driver"
    This reverts commit a47388743451e8913911b9568d6b1a8bcaa03ce8.

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -25,8 +25,6 @@
 #include <linux/seq_file.h>
 
 #include "mdss_dsi.h"
-#include "mdss_livedisplay.h"
-
 #include <linux/msm_mdp.h>
 #include <asm/uaccess.h>
 
@@ -331,7 +329,7 @@ u32 mdss_dsi_panel_cmd_read(struct mdss_dsi_ctrl_pdata *ctrl, char cmd0,
 	return 0;
 }
 
-void mdss_dsi_panel_cmds_send(struct mdss_dsi_ctrl_pdata *ctrl,
+static void mdss_dsi_panel_cmds_send(struct mdss_dsi_ctrl_pdata *ctrl,
 			struct dsi_panel_cmds *pcmds)
 {
 	struct dcs_cmd_req cmdreq;
@@ -849,12 +847,6 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
    // rt4532_set();
    ftxxxx_ts_resume();//resume touch
 
-#ifdef CONFIG_YULONG_COLOR
-	color_enhancement_impl_apply();
-#endif
-
-	mdss_livedisplay_update(ctrl, MODE_UPDATE_ALL);
-
 end:
 	pinfo->blank_state = MDSS_PANEL_BLANK_UNBLANK;
 	pr_debug("%s:-\n", __func__);
@@ -1033,7 +1025,7 @@ static void mdss_dsi_parse_trigger(struct device_node *np, char *trigger,
 }
 
 
-int mdss_dsi_parse_dcs_cmds(struct device_node *np,
+static int mdss_dsi_parse_dcs_cmds(struct device_node *np,
 		struct dsi_panel_cmds *pcmds, char *cmd_key, char *link_key)
 {
 	const char *data;
@@ -1959,8 +1951,6 @@ static int mdss_panel_parse_dt(struct device_node *np,
 	mdss_dsi_parse_panel_horizintal_line_idle(np, ctrl_pdata);
 
 	mdss_dsi_parse_dfps_config(np, ctrl_pdata);
-
-	mdss_livedisplay_parse_dt(np, pinfo);
 
 	return 0;
 
