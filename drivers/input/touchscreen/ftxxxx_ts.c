c9546de96ec556fe5f3901e4719a6a47601c7eb6
LuK1337 <priv.luk@gmail.com>
2016-04-24 00:23:15 +0200

    drivers: tfxxxx: Remove proximity check
    Change-Id: I70e982e92da2b23f6f08ac616d8030ca22f7002b

--- drivers/input/touchscreen/ftxxxx_ts.c
+++ drivers/input/touchscreen/ftxxxx_ts.c
@@ -526,8 +526,6 @@ static void check_gesture(struct ftxxxx_ts_data *data, int gesture_id)
 	bool Ps_status = false;
 
 //	printk(KERN_EMERG "[Focal][Touch] %s :  gesture_id = 0x%x\n ", __func__, gesture_id);
-		if(!ftxxxx_ts->cover_mode_states)
-			Ps_status = proximity_check_status();
 	if (!Ps_status) {
 		switch (gesture_id) {
 		/* ++++ touch gesture mode support part in ZE500CL ++++ */
