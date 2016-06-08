aaa88016b141e2ea862c913a35d23290db71358f
Christopher R. Palmer <crpalmer@gmail.com>
2016-06-08 17:35:56 +0200

    cluster-plug: Squash update
    Change-Id: I5d8b31f604b80d70b13ec797a21d5d27f28a014f

--- arch/arm/hotplug/cluster_plug.c
+++ arch/arm/hotplug/cluster_plug.c
@@ -2,7 +2,7 @@
  * Cluster-plug CPU Hotplug Driver
  * Designed for homogeneous ARM big.LITTLE systems
  *
- * Copyright 2015 Sultan Qasim Khan
+ * Copyright (C) 2015-2016 Sultan Qasim Khan and Christopher R. Palmer
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -23,43 +23,45 @@
 #include <linux/input.h>
 #include <linux/cpufreq.h>
 
-#ifdef CONFIG_POWERSUSPEND
-#include <linux/powersuspend.h>
-#endif
-
-#ifdef CONFIG_HAS_EARLYSUSPEND
-#include <linux/earlysuspend.h>
-#endif
-
-//#define DEBUG_CLUSTER_PLUG
-#undef DEBUG_CLUSTER_PLUG
-
-#define CLUSTER_PLUG_MAJOR_VERSION	1
+#define CLUSTER_PLUG_MAJOR_VERSION	2
 #define CLUSTER_PLUG_MINOR_VERSION	0
 
-#define DEF_HYSTERESIS			(10)
-#define DEF_LOAD_THRESH			(70)
-#define DEF_SAMPLING_MS			(200)
+#define DEF_LOAD_THRESH_DOWN		20
+#define DEF_LOAD_THRESH_UP		80
+#define DEF_SAMPLING_MS			50
+#define DEF_VOTE_THRESHOLD		3
 
-static DEFINE_MUTEX(cluster_plug_mutex);
+#define N_BIG_CPUS			4
+#define N_LITTLE_CPUS			4
+
+static DEFINE_MUTEX(cluster_plug_work_mutex);
+static DEFINE_MUTEX(cluster_plug_parameters_mutex);
 static struct delayed_work cluster_plug_work;
 static struct workqueue_struct *clusterplug_wq;
 
-static unsigned int cluster_plug_active = 0;
-module_param(cluster_plug_active, uint, 0664);
+static unsigned int load_threshold_down = DEF_LOAD_THRESH_DOWN;
+module_param(load_threshold_down, uint, 0664);
 
-static unsigned int hysteresis = DEF_HYSTERESIS;
-module_param(hysteresis, uint, 0664);
-
-static unsigned int load_threshold = DEF_LOAD_THRESH;
-module_param(load_threshold, uint, 0664);
+static unsigned int load_threshold_up = DEF_LOAD_THRESH_UP;
+module_param(load_threshold_up, uint, 0664);
 
 static unsigned int sampling_time = DEF_SAMPLING_MS;
 module_param(sampling_time, uint, 0664);
 
-static unsigned int cur_hysteresis = DEF_HYSTERESIS;
+static unsigned int vote_threshold_down = DEF_VOTE_THRESHOLD;
+module_param(vote_threshold_down, uint, 0664);
 
-static bool suspended = false;
+static unsigned int vote_threshold_up = DEF_VOTE_THRESHOLD;
+module_param(vote_threshold_up, uint, 0664);
+
+static ktime_t last_action;
+
+static bool active = false;
+static bool little_cluster_enabled = true;
+static bool low_power_mode = false;
+
+static unsigned int vote_up = 0;
+static unsigned int vote_down = 0;
 
 struct cp_cpu_info {
 	u64 prev_cpu_wall;
@@ -68,187 +70,293 @@ struct cp_cpu_info {
 
 static DEFINE_PER_CPU(struct cp_cpu_info, cp_info);
 
-static unsigned int calculate_loaded_cpus(void)
+static bool is_big_cpu(unsigned int cpu)
+{
+	return cpu < N_BIG_CPUS;
+}
+
+static bool is_little_cpu(unsigned int cpu)
+{
+	return !is_big_cpu(cpu);
+}
+
+static unsigned int get_delta_cpu_load_and_update(unsigned int cpu)
+{
+	u64 cur_wall_time, cur_idle_time;
+	unsigned int wall_time, idle_time;
+	struct cp_cpu_info *l_cp_info = &per_cpu(cp_info, cpu);
+
+	/* last parameter 0 means that IO wait is considered idle */
+	cur_idle_time = get_cpu_idle_time(cpu, &cur_wall_time, 0);
+
+	wall_time = (unsigned int)
+		(cur_wall_time - l_cp_info->prev_cpu_wall);
+	l_cp_info->prev_cpu_wall = cur_wall_time;
+
+	idle_time = (unsigned int)
+		(cur_idle_time - l_cp_info->prev_cpu_idle);
+	l_cp_info->prev_cpu_idle = cur_idle_time;
+
+	if (unlikely(!wall_time || wall_time < idle_time))
+		return 100;
+	else
+		return 100 * (wall_time - idle_time) / wall_time;
+}
+
+static unsigned int get_num_loaded_big_cpus(void)
 {
 	unsigned int cpu;
 	unsigned int loaded_cpus = 0;
-	struct cp_cpu_info *l_cp_info;
 
-	for_each_online_cpu(cpu) {
-		u64 cur_wall_time, cur_idle_time;
-		unsigned int wall_time, idle_time;
-		unsigned int cpu_load;
-		l_cp_info = &per_cpu(cp_info, cpu);
-
-		/* last parameter 0 means that IO wait is considered idle */
-		cur_idle_time = get_cpu_idle_time(cpu, &cur_wall_time, 0);
-
-		wall_time = (unsigned int)
-			(cur_wall_time - l_cp_info->prev_cpu_wall);
-		l_cp_info->prev_cpu_wall = cur_wall_time;
-
-		idle_time = (unsigned int)
-			(cur_idle_time - l_cp_info->prev_cpu_idle);
-		l_cp_info->prev_cpu_idle = cur_idle_time;
-
-		if (unlikely(!wall_time || wall_time < idle_time))
-			continue;
-
-		cpu_load = 100 * (wall_time - idle_time) / wall_time;
-
-		if (cpu_load > load_threshold)
-			loaded_cpus += 1;
+	for_each_possible_cpu(cpu) {
+		if (is_big_cpu(cpu)) {
+			unsigned int cpu_load = get_delta_cpu_load_and_update(cpu);
+			/* If a cpu is offline, assume it was loaded and forced offline */
+			if (!cpu_online(cpu) || cpu_load > load_threshold_up)
+				loaded_cpus += 1;
+		}
 	}
 
 	return loaded_cpus;
 }
 
-static void __ref plug_clusters(bool enable_little)
+static unsigned int get_num_unloaded_little_cpus(void)
 {
 	unsigned int cpu;
-	int ret;
-	bool powerhal_override = false;
+	unsigned int unloaded_cpus = 0;
 
-#ifdef DEBUG_CLUSTER_PLUG
-	if (enable_little) pr_info("enabling little cluster\n");
-	else pr_info("disabling little cluster\n");
-#endif
-
-	for_each_present_cpu(cpu) {
-		if (cpu < 4 || enable_little) {
-			if (cpu_online(cpu)) continue;
-			if (powerhal_override && cpu < 4) continue;
-			ret = cpu_up(cpu);
-
-			/* PowerHAL may force big cores offline */
-			if (ret == -EPERM && cpu < 4) {
-				enable_little = true;
-				powerhal_override = true;
-			}
-		} else {
-			cpu_down(cpu);
+	for_each_possible_cpu(cpu) {
+		if (is_little_cpu(cpu)) {
+			unsigned int cpu_load = get_delta_cpu_load_and_update(cpu);
+			/* If a little big is offline, treat it as unloaded */
+			if (!cpu_online(cpu) || cpu_load < load_threshold_down)
+				unloaded_cpus += 1;
 		}
 	}
+
+	return unloaded_cpus;
+}
+
+static void __ref enable_big_cluster(void)
+{
+	unsigned int cpu;
+	unsigned int num_up = 0;
+
+	for_each_present_cpu(cpu) {
+		if (is_big_cpu(cpu) && !cpu_online(cpu)) {
+			cpu_up(cpu);
+			num_up++;
+		}
+	}
+
+	pr_info("cluster_plug: %d big cpus enabled\n", num_up);
+}
+
+static void disable_big_cluster(void)
+{
+	unsigned int cpu;
+	unsigned int num_down = 0;
+
+	for_each_present_cpu(cpu) {
+		if (is_big_cpu(cpu) && cpu_online(cpu)) {
+			cpu_down(cpu);
+			num_down++;
+		}
+	}
+
+	pr_info("cluster_plug: %d big cpus disabled\n", num_down);
+}
+
+static void __ref enable_little_cluster(void)
+{
+	unsigned int cpu;
+	unsigned int num_up = 0;
+
+	for_each_present_cpu(cpu) {
+		if (is_little_cpu(cpu) && !cpu_online(cpu)) {
+			cpu_up(cpu);
+			num_up++;
+		}
+	}
+
+	pr_info("cluster_plug: %d little cpus enabled\n", num_up);
+}
+
+static void disable_little_cluster(void)
+{
+	unsigned int cpu;
+	unsigned int num_down = 0;
+
+	for_each_present_cpu(cpu) {
+		if (is_little_cpu(cpu) && cpu_online(cpu)) {
+			cpu_down(cpu);
+			num_down++;
+		}
+	}
+
+	pr_info("cluster_plug: %d little cpus disabled\n", num_down);
+}
+
+static void reset_cluster_state(void)
+{
+	mutex_lock(&cluster_plug_work_mutex);
+	if (!active) {
+		enable_big_cluster();
+		enable_little_cluster();
+	} else if (low_power_mode) {
+		enable_little_cluster();
+		disable_big_cluster();
+	} else {
+		enable_big_cluster();
+		disable_little_cluster();
+	}
+	mutex_unlock(&cluster_plug_work_mutex);
+}
+
+static void queue_cluster_plug_work_if_needed(void)
+{
+	if (active && !low_power_mode)
+		queue_delayed_work(clusterplug_wq, &cluster_plug_work, msecs_to_jiffies(sampling_time));
+}
+
+static void cluster_plug_perform(void)
+{
+	unsigned int loaded_cpus = get_num_loaded_big_cpus();
+	unsigned int unloaded_cpus = get_num_unloaded_little_cpus();
+	ktime_t now = ktime_get();
+
+	pr_debug("cluster-plug: little %d loaded: %u unloaded: %u votes %d / %d\n",
+		little_cluster_enabled, loaded_cpus, unloaded_cpus, vote_up, vote_down);
+
+	if (ktime_to_ms(ktime_sub(now, last_action)) > 5*sampling_time) {
+		pr_info("cluster_plug: ignoring old ts %lld\n",
+			ktime_to_ms(ktime_sub(now, last_action)));
+		vote_up = vote_down = 0;
+	} else {
+		if (loaded_cpus >= N_BIG_CPUS-1)
+			vote_up++;
+		else if (vote_up > 0)
+			vote_up--;
+
+		if (unloaded_cpus >= N_LITTLE_CPUS-1)
+			vote_down++;
+		else if (vote_down > 0)
+			vote_down--;
+	}
+
+	if (vote_up > vote_threshold_up) {
+		if (!little_cluster_enabled) {
+			enable_little_cluster();
+			little_cluster_enabled = true;
+		}
+		vote_up = vote_threshold_up;
+		vote_down = 0;
+	} else if (!vote_up && vote_down > vote_threshold_down) {
+		if (little_cluster_enabled) {
+			disable_little_cluster();
+			little_cluster_enabled = false;
+		}
+		vote_down = vote_threshold_down;
+	}
+
+	last_action = now;
 }
 
 static void __ref cluster_plug_work_fn(struct work_struct *work)
 {
-	unsigned int loaded_cpus, online_cpus;
+	mutex_lock(&cluster_plug_work_mutex);
+	cluster_plug_perform();
+	mutex_unlock(&cluster_plug_work_mutex);
 
-	if (cluster_plug_active) {
-		online_cpus = num_online_cpus();
-		loaded_cpus = calculate_loaded_cpus();
-#ifdef DEBUG_CLUSTER_PLUG
-		pr_info("loaded_cpus: %u\n", loaded_cpus);
-#endif
-
-		if (!suspended) {
-			if (loaded_cpus >= 3) {
-				cur_hysteresis = hysteresis;
-				if (online_cpus <= 4)
-					plug_clusters(true);
-			} else if (cur_hysteresis > 0) {
-				cur_hysteresis -= 1;
-			} else {
-				plug_clusters(false);
-			}
-		}
-	}
-
-	queue_delayed_work(clusterplug_wq, &cluster_plug_work,
-		msecs_to_jiffies(sampling_time));
+	queue_cluster_plug_work_if_needed();
 }
 
-#if defined(CONFIG_POWERSUSPEND) || defined(CONFIG_HAS_EARLYSUSPEND)
-
-#ifdef CONFIG_POWERSUSPEND
-static void __ref cluster_plug_suspend(struct power_suspend *handler)
-#else
-static void __ref cluster_plug_suspend(struct early_suspend *handler)
-#endif
+static int __ref active_show(char *buf,
+			const struct kernel_param *kp __attribute__ ((unused)))
 {
-	if (cluster_plug_active) {
-		int cpu;
+	return snprintf(buf, PAGE_SIZE, "%d", active);
+}
 
+static int __ref active_store(const char *buf,
+			const struct kernel_param *kp __attribute__ ((unused)))
+{
+	int ret, value;
+
+	ret = kstrtoint(buf, 0, &value);
+	if (ret == 0) {
+		mutex_lock(&cluster_plug_parameters_mutex);
+
+		cancel_delayed_work(&cluster_plug_work);
 		flush_workqueue(clusterplug_wq);
 
-		mutex_lock(&cluster_plug_mutex);
-		suspended = true;
-		mutex_unlock(&cluster_plug_mutex);
+		active = (value != 0);
+		reset_cluster_state();
 
-		// put rest of the cores to sleep unconditionally!
-		for_each_online_cpu(cpu) {
-			if (cpu != 0)
-				cpu_down(cpu);
-		}
+		mutex_unlock(&cluster_plug_parameters_mutex);
+
+		queue_cluster_plug_work_if_needed();
 	}
+
+	return ret;
 }
 
-#ifdef CONFIG_POWERSUSPEND
-static void __ref cluster_plug_resume(struct power_suspend *handler)
-#else
-static void __ref cluster_plug_resume(struct early_suspend *handler)
-#endif
+static const struct kernel_param_ops param_ops_active = {
+	.set = active_store,
+	.get = active_show
+};
+
+module_param_cb(active, &param_ops_active, &active, 0664);
+
+static int __ref low_power_mode_show(char *buf,
+			const struct kernel_param *kp __attribute__ ((unused)))
 {
-
-	if (cluster_plug_active) {
-		int cpu;
-
-		mutex_lock(&cluster_plug_mutex);
-		cur_hysteresis = hysteresis;
-		suspended = false;
-		mutex_unlock(&cluster_plug_mutex);
-
-		for_each_possible_cpu(cpu) {
-			if (cpu == 0)
-				continue;
-			cpu_up(cpu);
-		}
-	}
-	queue_delayed_work_on(0, clusterplug_wq, &cluster_plug_work,
-		msecs_to_jiffies(10));
+	return snprintf(buf, PAGE_SIZE, "%d", low_power_mode);
 }
 
-#endif /* CONFIG_POWERSUSPEND) || CONFIG_HAS_EARLYSUSPEND */
+static int __ref low_power_mode_store(const char *buf,
+			const struct kernel_param *kp __attribute__ ((unused)))
+{
+	int ret, value;
 
-#ifdef CONFIG_POWERSUSPEND
-static struct power_suspend cluster_plug_power_suspend_driver = {
-	.suspend = cluster_plug_suspend,
-	.resume = cluster_plug_resume,
-};
-#endif  /* CONFIG_POWERSUSPEND */
+	ret = kstrtoint(buf, 0, &value);
+	if (ret == 0) {
+		mutex_lock(&cluster_plug_parameters_mutex);
 
-#ifdef CONFIG_HAS_EARLYSUSPEND
-static struct early_suspend cluster_plug_early_suspend_driver = {
-        .level = EARLY_SUSPEND_LEVEL_DISABLE_FB + 10,
-        .suspend = cluster_plug_suspend,
-        .resume = cluster_plug_resume,
+		cancel_delayed_work(&cluster_plug_work);
+		flush_workqueue(clusterplug_wq);
+
+		low_power_mode = (value != 0);
+		reset_cluster_state();
+
+		mutex_unlock(&cluster_plug_parameters_mutex);
+
+		queue_cluster_plug_work_if_needed();
+	}
+
+	return ret;
+}
+
+static const struct kernel_param_ops param_ops_low_power_mode = {
+	.set = low_power_mode_store,
+	.get = low_power_mode_show
 };
-#endif	/* CONFIG_HAS_EARLYSUSPEND */
+
+module_param_cb(low_power_mode, &param_ops_low_power_mode, &low_power_mode, 0664);
 
 int __init cluster_plug_init(void)
 {
-	pr_info("cluster_plug: version %d.%d by sultanqasim\n",
+	pr_info("cluster_plug: version %d.%d by sultanqasim and crpalmer\n",
 		 CLUSTER_PLUG_MAJOR_VERSION,
 		 CLUSTER_PLUG_MINOR_VERSION);
-
-#ifdef CONFIG_POWERSUSPEND
-	register_power_suspend(&cluster_plug_power_suspend_driver);
-#endif
-#ifdef CONFIG_HAS_EARLYSUSPEND
-	register_early_suspend(&cluster_plug_early_suspend_driver);
-#endif
 
 	clusterplug_wq = alloc_workqueue("clusterplug",
 				WQ_HIGHPRI | WQ_UNBOUND, 1);
 	INIT_DELAYED_WORK(&cluster_plug_work, cluster_plug_work_fn);
-	queue_delayed_work_on(0, clusterplug_wq, &cluster_plug_work,
-		msecs_to_jiffies(10));
 
 	return 0;
 }
 
-MODULE_AUTHOR("Sultan Qasim Khan <sultanqasim@gmail.com>");
+MODULE_AUTHOR("Sultan Qasim Khan <sultanqasim@gmail.com> and Christopher R. Palmer <crpalmer@gmail.com>");
 MODULE_DESCRIPTION("'cluster_plug' - A cluster based hotplug for homogeneous"
         "ARM big.LITTLE systems where the big cluster is preferred."
 );
