da5c9e4701c4386a05ac8d430bc64259d12c1014
luca020400 <luca.stefani.ge1@gmail.com>
2016-06-14 23:45:51 +0200

    [DNM] arch/arm: dts: Disable lpm-workarounds
    * Also go back to lollipop lpm-workarounds
    
    Change-Id: I12087af740900d981ca226e6f52126114c770a24

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -46,6 +46,7 @@
 #include <trace/events/power.h>
 #define CREATE_TRACE_POINTS
 #include <trace/events/trace_msm_low_power.h>
+#include "../../arch/arm/mach-msm/clock.h"
 
 #define SCLK_HZ (32768)
 #define SCM_HANDOFF_LOCK_ID "S:7"
@@ -214,13 +215,7 @@ int set_l2_mode(struct low_power_ops *ops, int mode, bool notify_rpm)
 		lpm = MSM_SPM_MODE_DISABLED;
 		break;
 	}
-
-	/* Do not program L2 SPM enable bit. This will be set by TZ */
-	if (lpm_wa_get_skip_l2_spm())
-		rc = msm_spm_config_low_power_mode_addr(ops->spm, lpm,
-							true);
-	else
-		rc = msm_spm_config_low_power_mode(ops->spm, lpm, true);
+	rc = msm_spm_config_low_power_mode(ops->spm, lpm, true);
 
 	if (rc)
 		pr_err("%s: Failed to set L2 low power mode %d, ERR %d",
@@ -260,13 +255,7 @@ static int cpu_power_select(struct cpuidle_device *dev,
 	if (sleep_disabled)
 		return 0;
 
-	/*
-	 * TODO:
-	 * Assumes event happens always on Core0. Need to check for validity
-	 * of this scenario on cluster low power modes
-	 */
-	if (!dev->cpu)
-		next_event_us = (uint32_t)(ktime_to_us(get_next_event_time()));
+	next_event_us = (uint32_t)(ktime_to_us(get_next_event_time()));
 
 	for (i = 0; i < cpu->nlevels; i++) {
 		struct lpm_cpu_level *level = &cpu->levels[i];
@@ -327,7 +316,7 @@ static int cpu_power_select(struct cpuidle_device *dev,
 		}
 	}
 
-	if (modified_time_us && !dev->cpu)
+	if (modified_time_us)
 		msm_pm_set_timer(modified_time_us);
 
 	return best_level;
@@ -597,6 +586,13 @@ static void cluster_unprepare(struct lpm_cluster *cluster,
 	level = &cluster->levels[cluster->last_level];
 	if (level->notify_rpm) {
 		msm_rpm_exit_sleep();
+
+		/* If RPM bumps up CX to turbo, unvote CX turbo vote
+		 * during exit of rpm assisted power collapse to
+		 * reduce the power impact
+		 */
+
+		lpm_wa_cx_unvote_send();
 		msm_mpm_exit_sleep(from_idle);
 	}
 
@@ -827,8 +823,6 @@ static void register_cpu_lpm_stats(struct lpm_cpu *cpu,
 
 	lpm_stats_config_level("cpu", level_name, cpu->nlevels,
 			parent->stats, &parent->child_cpus);
-
-	kfree(level_name);
 }
 
 static void register_cluster_lpm_stats(struct lpm_cluster *cl,
@@ -899,6 +893,7 @@ static int lpm_suspend_enter(suspend_state_t state)
 	}
 	cpu_prepare(cluster, idx, false);
 	cluster_prepare(cluster, cpumask, idx, false);
+        clock_debug_print_enabled();
 	msm_cpu_pm_enter_sleep(cluster->cpu->levels[idx].mode, false);
 	cluster_unprepare(cluster, cpumask, idx, false);
 	cpu_unprepare(cluster, idx, false);
